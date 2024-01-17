3b65c417b5399602061fba2a157525586ea0938f
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-01-17 21:03:06 +0100

    fix deps

--- src/infrastructure/web/start_server.rs
+++ src/infrastructure/web/start_server.rs
@@ -6,10 +6,9 @@ use crate::infrastructure::web::routes::routes_v1;
 pub async fn start_server() {
     let state = MyService::new();
     let app: Router = Router::new().nest("/api/v1", routes_v1()).with_state(state);
-    let addr = "0.0.0.0:8080".parse().expect("Failed to parse address");
 
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app)
         .await
         .expect("Failed to run server")
 }
