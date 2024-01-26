7473f29b55f20b4e96a271add8e1ece399db87ae
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-01-26 18:11:45 +0100

    chore: basic tests, try sesions

--- src/infrastructure/web/mod.rs
+++ src/infrastructure/web/mod.rs
@@ -1,3 +1,30 @@
 pub mod controller;
 pub mod routes;
-pub mod start_server;
+
+use axum::Router;
+
+use crate::application::service::Bingokta;
+use crate::infrastructure::web::routes::{routes_default, routes_v1};
+
+use tower_sessions::{Expiry, MemoryStore, SessionManagerLayer};
+
+pub async fn start_server() {
+    let session_store = MemoryStore::default();
+    let session_layer = SessionManagerLayer::new(session_store)
+        .with_secure(false)
+        .with_expiry(Expiry::OnInactivity(
+            tower_sessions::cookie::time::Duration::weeks(55),
+        ));
+
+    let state = Bingokta::new();
+    let app: Router = Router::new()
+        .nest("/api/v1", routes_v1())
+        .with_state(state)
+        .layer(session_layer)
+        .nest("/", routes_default());
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to run server")
+}
--- src/infrastructure/web/routes.rs
+++ src/infrastructure/web/routes.rs
@@ -1,14 +1,33 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, put},
+    Router,
+};
 
 use crate::{
     application::service::Bingokta,
-    infrastructure::web::controller::{get_file, ping},
+    infrastructure::web::controller::{get_file, number, ping},
 };
 
 pub fn routes_v1() -> Router<Bingokta> {
-    Router::<Bingokta>::new().route("/get_file", get(get_file))
+    Router::<Bingokta>::new()
+        .route("/get_file", get(get_file))
+        .route("/number/:number", put(number))
 }
 
-pub fn routes_default() -> Router<Bingokta> {
-    Router::<Bingokta>::new().route("/ping", get(ping))
+pub fn routes_default() -> Router {
+    Router::new().route("/ping", get(ping))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::http::StatusCode;
+    use axum_test::TestServer;
+
+    #[tokio::test]
+    async fn the_real_deal() {
+        let server = TestServer::new(routes_default()).unwrap();
+        let response = server.get("/ping").await;
+        assert_eq!(response.status_code(), StatusCode::NO_CONTENT);
+    }
 }
