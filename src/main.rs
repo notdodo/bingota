5f219cd381feddc1c9521cba4d8c4bb5d27996a3
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2024-01-27 12:38:04 +0100

    enh: rust wf

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use bingokta::{infrastructure::web::start_server, init_otel};
 #[tokio::main]
 async fn main() {
     let _guard = init_otel();
-    tracing::info!("Bingoktà started");
+    tracing::info!("Bingoktà started!");
     start_server().await;
 }
